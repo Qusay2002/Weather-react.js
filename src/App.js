0df3a5a044437e5c8e9362ca346f37797111d0c3
Qusay2002 <ateyyatq@gmail.com>
2024-01-01 15:10:42 +0300

    upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import React from 'react';
+import Weather from './Weather';
+
+function App() {
+  return (
+    <div className="App">
+      <Weather />
+    </div>
+  );
+{/*const routes = createHashRouter([{
+  
+}])
+*/}
+}
+
+export default App;
